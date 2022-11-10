f723f6de3e35c9f45902bfb4ff1017a03a7e1642
gustavld <gustav.ledous@gmail.com>
2022-11-10 14:32:30 +0100

    Added friction model

--- system/model.py
+++ system/model.py
@@ -4,15 +4,15 @@ import numpy as np
 
 from dataclasses import dataclass
 
-dT = 0.0001
+dT = 0.00001
 
 L = 5 / 1000  # vad ska det vara här? Läste 28 mH nånstans
 B = 0.1  # 0 friktion beroende på varvtal
 k_lambda = 0.2
-R = 0.5 * 4
+R = 0.5
 
 g = 9.82
-m = 10
+m = 100
 utväxlingskonstant = 9
 verkningsgrad = 0.3
 J = 0.00024  # moment konstant för motorn + system???
@@ -22,8 +22,12 @@ J = 0.00024  # moment konstant för motorn + system???
 class SimulationResult:
     current: np.ndarray
     omega: np.ndarray
+    pos: np.ndarray
 
     time: np.ndarray
+
+    t_dev: np.ndarray
+    t_last: np.ndarray
 
     voltages: Dict[str, np.ndarray]
 
@@ -31,14 +35,19 @@ class SimulationResult:
 def simulate(seconds: int, target_current: int) -> SimulationResult:
     N = int(seconds / dT)
 
+    # Tillståndsvariabler
     I = np.zeros(N)
     omega = np.zeros(N)
+    pos = np.zeros(N)
 
     V = np.zeros(N)
 
     vL = np.zeros(N)
     vR = np.zeros(N)
     vEa = np.zeros(N)
+
+    T_dev = np.zeros(N)
+    T_last = np.zeros(N)
 
     for i in range(1, N):
         V[i] = 24 if I[i - 1] < target_current else 0
@@ -52,29 +61,46 @@ def simulate(seconds: int, target_current: int) -> SimulationResult:
         I[i] = I[i - 1] + dT * dI
 
         applied_force = m * (g + 0)
-        T_last_min, T_last_max = screw_torque_with_friction(omega[i - 1], applied_force)
+        # T_last_max, T_last_min = screw_torque_with_friction(omega[i - 1], applied_force)
 
-        T_last_min, T_last_max = T_last_min / utväxlingskonstant, T_last_max / utväxlingskonstant
+        # T_last_min, T_last_max = T_last_min / utväxlingskonstant, T_last_max / utväxlingskonstant
 
-        T_dev = k_lambda * I[i - 1]
+        T_dev[i] = k_lambda * I[i - 1]
 
-        is_dynamic = T_last_min <= T_dev <= T_last_max
-
+        is_dynamic = abs(omega[i - 1]) > 1
         if is_dynamic:
-            dOmega = (T_dev - (B * omega[i - 1] + m * g / utväxlingskonstant / verkningsgrad)) / J
-        else:
-            dOmega = 0
+            # dynamisk friction
+            T_last[i] = screw_torque_with__dynamic_friction(omega[i - 1], applied_force)
 
-        # print(dOmega)
+        else:
+            # static friction
+            T_last_max, T_last_min = screw_torque_with_static_friction(applied_force)
+
+            if T_last_min <= T_dev[i] <= T_last_max:
+                T_last[i] = T_dev[i]
+            else:
+                if T_dev[i] > T_last_max:
+                    T_last[i] = T_dev[i] - T_last_max
+                else:
+                    T_last = T_dev[i] - T_last_min
+
+        T_last[i] = T_last[i] / utväxlingskonstant
+
+        dOmega = (T_dev[i] - T_last[i]) / J
 
         omega[i] = omega[i - 1] + dT * dOmega
 
-    t = np.linspace(0, N * dT, N)
+        dpos = omega[i] / 1800
+        pos[i] = pos[i - 1] + dT * dpos
 
+    t = np.linspace(0, N * dT, N)
     return SimulationResult(
         current=I,
         omega=omega,
         time=t,
+        t_dev=T_dev,
+        t_last=T_last,
+        pos=pos,
         voltages={
             "V_L": vL,
             "V_R": vR,
@@ -84,7 +110,7 @@ def simulate(seconds: int, target_current: int) -> SimulationResult:
     )
 
 
-def screw_torque_with_friction(w, F):  # [Tmax,T,Tmin]
+def screw_torque_with__dynamic_friction(w, F):  # [Tmax,T,Tmin]
     screw_outer_dia = 0.025
     screw_inner_dia = 0.020
     pitch = 0.01  # m/revulution
@@ -100,13 +126,29 @@ def screw_torque_with_friction(w, F):  # [Tmax,T,Tmin]
 
     R_eff = (screw_outer_dia + screw_inner_dia) / 4  # screw effective radius
 
-    if abs(w) > w_min_dynamic:
-        # dynamic friction
-        T = T_no_friction + (abs(F) * R_eff * friction_coef_dynamic + no_load_friction_torque_dynamic) * np.sign(w)
-        Tmax = T
-        Tmin = T
-    else:
-        # static friction
-        Tmax = T_no_friction + (abs(F) * R_eff * friction_coef_static + no_load_friction_torque_static)
-        Tmin = T_no_friction - (abs(F) * R_eff * friction_coef_static + no_load_friction_torque_static)
+    # dynamic friction
+    T = T_no_friction + (abs(F) * R_eff * friction_coef_dynamic + no_load_friction_torque_dynamic) * np.sign(w)
+    return T
+
+
+def screw_torque_with_static_friction(F):  # [Tmax,T,Tmin]
+
+    screw_outer_dia = 0.025
+    screw_inner_dia = 0.020
+    friction_coef_static = 0.15
+    friction_coef_dynamic = 0.10
+
+    no_load_friction_torque_static = 0.03
+    no_load_friction_torque_dynamic = 0.02
+
+    pitch = 0.01  # m/revulution
+    R_eff = (screw_outer_dia + screw_inner_dia) / 4  # screw effective radius
+
+    pitch_rad = pitch / 2 / np.pi  # mm/rad
+
+    T_no_friction = F * pitch_rad
+
+    Tmax = T_no_friction + (abs(F) * R_eff * friction_coef_static + no_load_friction_torque_static)
+    Tmin = T_no_friction - (abs(F) * R_eff * friction_coef_static + no_load_friction_torque_static)
+
     return Tmax, Tmin
