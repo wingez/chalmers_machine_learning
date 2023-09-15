cbf9fd53144adfd0a6644f079e5c189d263ab75e
gustavld <gustav.ledous@gmail.com>
2023-09-15 09:11:30 +0200

    improved model

--- system/model.py
+++ system/model.py
@@ -9,8 +9,10 @@ from dataclasses import dataclass
 import random
 import bisect
 
+rand = np.random
+
 # Sannolikhet för fel per rotation
-fault_value = 0.00001
+fault_value = 0.0001
 
 # Set random seed
 random.seed(1)
@@ -59,8 +61,8 @@ filter_step = 20
 ##################
 
 # Regulalator controller parameters
-Kp = 1
-Ki = 0.0001
+Kp = 0.3
+Ki = 0.05
 
 # Modes the regulator can operate in
 REGULATOR = "regulator"
@@ -89,17 +91,25 @@ class SimulationResult:
     error: np.ndarray
     error_accumulated: np.ndarray
 
+    P_mek: np.ndarray
+    P_ut: np.ndarray
+    P_e: np.ndarray
+    E_broms: np.ndarray
+    g_effekt: np.ndarray
+
 
 def simulate(seconds: float, max_current: float, m: float, voltage_selector: str,
              target_function: Callable[[float], float]) -> SimulationResult:
     # Antal simuleringssteg
     N = int(seconds / dT)
 
-    # Tillståndsvariabler
+    # Tillståndsvariabler i motorn
     I = np.zeros(N)
+    # Varvtal på motorn i rad/s
     omega = np.zeros(N)
 
-    # Positionen är integralen av omega (med lite utväxlingskonstanter)
+    # Rörelse på skrivbordet i m, m/s, m/s2
+    # vel är proportionellt mot omega (med en utväxlingskonstant)
     pos = np.zeros(N)
     vel = np.zeros(N)
     acc = np.zeros(N)
@@ -130,6 +140,13 @@ def simulate(seconds: float, max_current: float, m: float, voltage_selector: str
     Error = np.zeros(N)
     ErrorAccumulated = np.zeros(N)
 
+    # Effekt
+    P_mek = np.zeros(N)
+    P_ut = np.zeros(N)
+    P_e = np.zeros(N)
+    E_broms = np.zeros(N)
+    g_effekt = np.zeros(N)
+
     # Loopa varje simuleringssteg
     for i in range(1, N):
 
@@ -139,7 +156,7 @@ def simulate(seconds: float, max_current: float, m: float, voltage_selector: str
 
         # Sensorfel
         # Fel gällande avlästa varv
-        if random.random() <= fault_value * omega[i - 1]:
+        if False:  # random.random() <= fault_value * omega[i - 1]:
             rev_fault = omega_sensorfault(omega[i - 1])
         else:
             rev_fault = 0
@@ -156,6 +173,8 @@ def simulate(seconds: float, max_current: float, m: float, voltage_selector: str
             last_omega = omega[i - 1] - rev_fault
             Error[i] = target_omega - last_omega
             ErrorAccumulated[i] = ErrorAccumulated[i - 1] + Error[i] * dT
+            if target_omega == 0:
+                ErrorAccumulated[i] = 0
 
             targetVoltage = Kp * Error[i] + Ki * ErrorAccumulated[i]
         else:
@@ -168,9 +187,9 @@ def simulate(seconds: float, max_current: float, m: float, voltage_selector: str
         # lägga till mätbrus 15mA, mätosäkerhet 1.5%
         last_current = I[i - 1]
         sensor_noise = (0.015 * last_current) + 0.015
-        I[i - 1] = last_current + rand.uniform(-sensor_noise, sensor_noise)
+        measured_current = last_current + rand.uniform(-sensor_noise, sensor_noise)
         # clamp current So that it dont exceed max-current. Requirement from LArs in mail
-        V[i] = targetVoltage if I[i - 1] < max_current else 0
+        V[i] = targetVoltage if abs(measured_current) < max_current else 0
 
         # Beräkna spänningarna över de olika delarna i motorn
         vR[i] = R * I[i - 1]
@@ -183,16 +202,21 @@ def simulate(seconds: float, max_current: float, m: float, voltage_selector: str
         # Beräkna t_last. Dvs motverkande vridmoment från lasten
         applied_force = m * (g + 0)
 
+        # Antag vi står still om omega är nära 0
         is_dynamic = abs(omega[i - 1]) > 1
         if is_dynamic:
             # dynamisk friction
             T_last[i] = screw_torque_with__dynamic_friction(omega[i - 1], applied_force)
+            T_last[i] = T_last[i] / transmission_reduction
 
         else:
             # static friction
             T_last_max, T_last_min = screw_torque_with_static_friction(applied_force)
+            T_last_max = T_last_max / transmission_reduction
+            T_last_min = T_last_min / transmission_reduction
 
             if T_last_min <= T_dev[i] <= T_last_max:
+                # If T_dev is within the range we should not move at all.
                 T_last[i] = T_dev[i]
             else:
                 if T_dev[i] > T_last_max:
@@ -200,27 +224,39 @@ def simulate(seconds: float, max_current: float, m: float, voltage_selector: str
                 else:
                     T_last[i] = T_last_min
 
-        T_last[i] = T_last[i] / transmission_reduction
+        # T_last har hitills beräknats på trapsgängstången.
+        # Multiplicera med utväxlingen i växellådan för att få vridmoment på motorn
+        # T_last[i] = T_last[i] / transmission_reduction
 
         # Beräkna strömderivatan
         dI = (V[i] - R * I[i - 1] - k_lambda * omega[i - 1]) / L
-        # Integrera strömmen för nästa spännigssteg
+        # Integrera strömmen för nästa simuleringssteg
         I[i] = I[i - 1] + dT * dI
 
         # Beräkna omega-derivatan
         dOmega = (T_dev[i] - T_last[i]) / J
-        # Integrera omega för nästa spännigssteg
+        # Integrera omega för nästa simuleringssteg
         omega[i] = omega[i - 1] + dT * dOmega
-
-        # Positionen är omega integrerat
-        vel[i] = omega[i] * rads_to_ms
-        pos[i] = pos[i - 1] + dT * vel[i]
 
         # vel och acc är proportionellt mot omega respektive dOmega
         acc[i] = dOmega * rads_to_ms
+        vel[i] = omega[i] * rads_to_ms
+
+        # Positionen är hastigheten integrerat
+        pos[i] = pos[i - 1] + dT * vel[i]
 
         # Beräkna genomsnittsspänning
         v_filt[i] = v_filt[i - 1] * (1 - dT / (filter_step * dT)) + V[i] * dT / (filter_step * dT)
+
+        # Elektrisk effekt
+        P_e[i] = V[i] * I[i]
+        # Mekanisk effekt
+        P_mek[i] = omega[i] * T_dev[i]
+        P_ut[i] = applied_force * vel[i]
+
+        E_broms[i] = (m * vel[i] ** 2) / 2 + (1 / 2) * J * omega[i] ** 2
+
+        g_effekt[i] = m * g * pos[i]  # gravitationseffekt, för neråt
 
     t = np.linspace(0, N * dT, N)
     return SimulationResult(
@@ -242,30 +278,45 @@ def simulate(seconds: float, max_current: float, m: float, voltage_selector: str
         },
         error=Error,
         error_accumulated=ErrorAccumulated,
+        P_mek=P_mek,
+        P_ut=P_ut,
+        P_e=P_e,
+        E_broms=E_broms,
+        g_effekt=g_effekt
     )
 
 
-# I dont really know what this does. This is all Lars's code
 def screw_torque_with__dynamic_friction(w, F):  # [T]
 
-    pitch_rad = pitch / 2 / np.pi  # mm/rad
-    T_no_friction = F * pitch_rad
+    pitch_rad = pitch / 2 / np.pi  # (Sträcka per radian)
+    T_no_friction = F * pitch_rad  # (N * m per radian)
 
-    R_eff = (screw_outer_dia + screw_inner_dia) / 4  # screw effective radius
+    # Radien till gängans mittpunkt
+    R_eff = (screw_outer_dia + screw_inner_dia) / 4
 
-    # dynamic friction
+    # Funktionen nedan ger summan av:
+    # Det friktionsfria vridmomentet.
+    # Vridmomentet av friktion från gängan under belastning.
+    # Det "passiva" vridmomentet som gäller oavsett belastning.
+    # Samtliga för det dynamiska fallet och med hänsyn till rotationsriktning.
+
     T = T_no_friction + (abs(F) * R_eff * friction_coef_dynamic + no_load_friction_torque_dynamic) * np.sign(w)
     return T
 
 
 def screw_torque_with_static_friction(F):  # [Tmax,T,Tmin]
 
-    R_eff = (screw_outer_dia + screw_inner_dia) / 4  # screw effective radius
+    R_eff = (screw_outer_dia + screw_inner_dia) / 4  # Gängradie
 
-    pitch_rad = pitch / 2 / np.pi  # mm/rad
+    pitch_rad = pitch / 2 / np.pi
 
     T_no_friction = F * pitch_rad
 
+    # Här beräknas båda relevanta 'gränser' för vad vridmomentet
+    # behöver överkomma för att gå över till det dynamiska stadiet
+    #
+    # Beräkningen sker på liknande sätt som ovan men utan rotationsriktning
+    # eftersom detta är två olika utfall
     Tmax = T_no_friction + (abs(F) * R_eff * friction_coef_static + no_load_friction_torque_static)
     Tmin = T_no_friction - (abs(F) * R_eff * friction_coef_static + no_load_friction_torque_static)
 
